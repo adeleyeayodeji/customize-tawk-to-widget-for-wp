af9fa6f941dcee2a99b451e524e40a43cb105744
adeleyeayodeji <biodunhi@gmail.com>
2024-06-24 08:18:48 +0100

    Added support for latest twak.to

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,14 +13,14 @@ let customiseTawkToWidget = () => {
         "topLeft"
       ) {
         //set left style 20px
-        element.style.left = "20px";
+        element.style.setProperty("left", "20px", "important");
         //set right style auto
-        element.style.right = "auto";
+        element.style.setProperty("right", "auto", "important");
       } else {
         //set right style 20px
-        element.style.right = "20px";
+        element.style.setProperty("right", "20px", "important");
         //set left style auto
-        element.style.left = "auto";
+        element.style.setProperty("left", "auto", "important");
       }
     });
   }
@@ -30,6 +30,25 @@ document.addEventListener(
   "DOMContentLoaded",
   () => {
     window.Tawk_API = window.Tawk_API || {};
+
+    // Set interval to check for .widget-visible or .widget-hidden class
+    window.widgetVisibilityCheck = setInterval(() => {
+      //check if .widget-visible exists
+      if (document.querySelector(".widget-visible")) {
+        //set advancetawktocustomise to display none to important
+        document
+          .querySelector(".widget-visible")
+          .style.setProperty("display", "none", "important");
+
+        //confirm .widget-visible is hidden then clear interval
+        if (
+          document.querySelector(".widget-visible").style.display === "none"
+        ) {
+          clearInterval(widgetVisibilityCheck);
+        }
+      }
+    }, 50);
+
     window.Tawk_API.onLoad = function () {
       //display advancetawktocustomise
       document.querySelector(".advancetawktocustomise").style.display = "block";
@@ -40,17 +59,52 @@ document.addEventListener(
     document
       .querySelector(".advancetawktocustomise-new-design")
       .addEventListener("click", () => {
+        //then toggle widget
         window.$_Tawk.toggle();
       });
 
     window.Tawk_API.onChatMinimized = function () {
       //fade in advancetawktocustomisebtn
       document.querySelector(".advancetawktocustomise").style.display = "block";
+      //hide widget
+      window.$_Tawk.hideWidget();
     };
 
     window.Tawk_API.onChatMaximized = function () {
       document.querySelector(".advancetawktocustomise").style.display = "none";
     };
+
+    //customise init
+    let initcustomise = () => {
+      //init
+      document.querySelector(".advancetawktocustomise").style.display = "block";
+      //hideWidget
+      window.$_Tawk.hideWidget();
+      //check element exist and set widget-hidden to block
+      if (document.querySelector(".widget-hidden")) {
+        document.querySelector(".widget-hidden").style.display = "block";
+      }
+      //check element exist and set widget-visible to none
+      if (document.querySelector(".widget-visible")) {
+        document.querySelector(".widget-visible").style.display = "block";
+      }
+    };
+
+    //set interval to check if api has onBeforeLoaded hasOwnProperty and is true
+    let tawkApiCheck = setInterval(() => {
+      if (
+        window.Tawk_API.hasOwnProperty("onBeforeLoaded") &&
+        window.Tawk_API.onBeforeLoaded
+      ) {
+        initcustomise();
+        clearInterval(tawkApiCheck);
+      }
+    }, 500);
   },
   false
 );
+
+//set interval to check if api has onBeforeLoaded hasOwnProperty and is true
+setInterval(() => {
+  customiseTawkToWidget();
+}, 50);
